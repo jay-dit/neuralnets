51b4421d82bcddd01dd74c187402c7f0a23a880a
AnnaKupri <69643094+jay-dit@users.noreply.github.com>
2023-11-04 08:57:50 +0300

    Update seminar4.py

--- src/seminar4.py
+++ src/seminar4.py
@@ -34,7 +34,7 @@ class Optimizer(ABC):
 class SGD(Optimizer):
     def step(self, w, d_w, learning_rate):
         # TODO Update W with d_W
-        pass
+        w -= d_w * learning_rate
 
 
 class Momentum(Optimizer):
@@ -45,14 +45,22 @@ class Momentum(Optimizer):
     def step(self, w, d_w, learning_rate):
         if self.velocity is None:
             self.velocity = np.zeros_like(d_w)
-        # TODO Update W with d_W and velocity
+        new_velocity = self.rho * self.velocity + d_w
+        w -= new_velocity * learning_rate
+        self.velocity = new_velocity
 
 
 class DropoutLayer(Layer):
+    def __init__(self, p=0.5):
+        self.p = p
+        self.mask = None
+        self.scale = None
+
     def forward(self, x: np.ndarray, train: bool = True) -> np.ndarray:
         if train:
-            # TODO zero mask in random X position and scale remains
-            pass
+            self.mask = np.random.random(size=x.shape) > self.p
+            self.scale = 1 / (1-self.p)
+            return x * self.mask * self.scale
         else:
             return x
 
@@ -62,10 +70,6 @@ class DropoutLayer(Layer):
     def params(self) -> dict:
         return {}
 
-    def __init__(self, p=0.5):
-        self.p = p
-        self.mask = None
-        self.scale = None
 
 
 class BatchNormLayer(Layer):
@@ -102,11 +106,13 @@ class BatchNormLayer(Layer):
     def forward(self, x: np.ndarray, train: bool = True) -> np.ndarray:
         self.num_examples = x.shape[0]
         if train:
-            # TODO Compute mean_x and var_x
+            self.mean_x = np.mean(x, axis=0, keepdims=True)
+            self.var_x = np.var(x, axis=0, keepdims=True)
             self._update_running_variables()
         else:
-            # TODO Copy mean_x and var_x from running variables
-            pass
+            self.mean_x = self.running_mean_x
+            self.var_x = self.running_var_x
+
 
         self.var_x += epsilon
         self.stddev_x = np.sqrt(self.var_x)
