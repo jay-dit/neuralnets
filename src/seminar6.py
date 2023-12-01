bee1cd3f16fe4663ca7da490cf9ae5a3a9443d38
AnnaKupri <69643094+jay-dit@users.noreply.github.com>
2023-12-01 22:51:58 +0300

    Seminar_6

--- src/seminar6.py
+++ src/seminar6.py
@@ -6,6 +6,8 @@ import shutil
 from urllib.request import urlretrieve
 
 import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
 import boto3
 import dotenv
 
@@ -15,7 +17,7 @@ PATH_TO_DATA = 'data/raw/cats_dogs_train'
 PATH_TO_MODEL = 'models/model_6'
 BUCKET_NAME = 'neuralnets2023'
 # todo fix your git user name and copy .env to project root
-YOUR_GIT_USER = 'labintsev'
+YOUR_GIT_USER = 'jay-dit'
 
 
 def download_data():
@@ -33,15 +35,136 @@ def download_data():
     else:
         print('Data is already extracted!')
 
+def filter_images():
+    num_skipped = 0
+    for folder_name in ("Cat", "Dog"):
+        folder_path = os.path.join(PATH_TO_DATA+"/PetImages", folder_name)
+        for fname in os.listdir(folder_path):
+            fpath = os.path.join(folder_path, fname)
+            try:
+                fobj = open(fpath, "rb")
+                is_jfif = tf.compat.as_bytes("JFIF") in fobj.peek(10)
+            finally:
+                fobj.close()
+
+            if not is_jfif:
+                num_skipped += 1
+                # Delete corrupted image
+                os.remove(fpath)
+
+    print("Deleted %d images" % num_skipped)
+
+def configure_data():
+    image_size = (180, 180)
+    batch_size = 32
+    train_ds, val_ds = tf.keras.utils.image_dataset_from_directory(
+        PATH_TO_DATA+"/PetImages",
+        validation_split=0.2,
+        subset="both",
+        seed=1337,
+        image_size=image_size,
+        batch_size=batch_size,
+    )
+
+
+
+    data_augmentation_layers_v1 = [
+        layers.RandomFlip("horizontal"),
+        layers.RandomRotation(0.1),
+    ]
+
+    data_augmentation = keras.Sequential(
+        [
+            layers.RandomFlip("horizontal"),
+            layers.RandomRotation(0.1),
+        ]
+    )
+    def data_augmentation(images):
+        for layer in data_augmentation_layers_v1:
+            images = layer(images)
+        return images
+    
+    train_ds = train_ds.map(
+        lambda img, label: (data_augmentation(img), label),
+        num_parallel_calls=tf.data.AUTOTUNE,
+    )
+
+    train_ds = train_ds.prefetch(tf.data.AUTOTUNE)
+    val_ds = val_ds.prefetch(tf.data.AUTOTUNE)
+
+    return train_ds, val_ds
+
 
 def make_model(input_shape, num_classes):
-    model = None
-    return model
+    inputs = tf.keras.Input(shape=input_shape)
+
+    # Entry block
+    x = tf.keras.layers.Rescaling(1.0 / 255)(inputs)
+    x = tf.keras.layers.Conv2D(128, 3, strides=2, padding="same")(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Activation("relu")(x)
+
+    previous_block_activation = x  # Set aside residual
+
+    for size in [256, 512, 728]:
+        x = tf.keras.layers.Activation("relu")(x)
+        x = tf.keras.layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = tf.keras.layers.BatchNormalization()(x)
+
+        x = tf.keras.layers.Activation("relu")(x)
+        x = tf.keras.layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = tf.keras.layers.BatchNormalization()(x)
+
+        x = tf.keras.layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+        # Project residual
+        residual = tf.keras.layers.Conv2D(size, 1, strides=2, padding="same")(
+            previous_block_activation
+        )
+        x = tf.keras.layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
+    x = tf.keras.layers.SeparableConv2D(1024, 3, padding="same")(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Activation("relu")(x)
+
+    x = tf.keras.layers.GlobalAveragePooling2D()(x)
+    if num_classes == 2:
+        activation = "sigmoid"
+        units = 1
+    else:
+        activation = "softmax"
+        units = num_classes
+
+    x = tf.keras.layers.Dropout(0.5)(x)
+    outputs = tf.keras.layers.Dense(units, activation=activation)(x)
+    return tf.keras.Model(inputs, outputs)
 
 
 def train():
     """Pipeline: Build, train and save model to models/model_6"""
     # Todo: Copy some code from seminar5 and https://keras.io/examples/vision/image_classification_from_scratch/
+    train_ds, val_ds = configure_data()
+    image_size = (180, 180)
+    batch_size = 64
+    model = make_model(input_shape=[*image_size, 3], num_classes=2)
+    epochs = 6
+
+    callbacks = [
+        tf.keras.callbacks.ModelCheckpoint("save_at_{epoch}.keras"),
+    ]
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(1e-3),
+        loss="binary_crossentropy",
+        metrics=["accuracy"],
+    )
+    model.fit(
+        train_ds,
+        epochs=epochs,
+        callbacks=callbacks,
+        validation_data=val_ds,
+    )
+    model.save(PATH_TO_MODEL)
     print('Training model')
 
 
@@ -69,16 +192,6 @@ def upload():
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        prog='src/seminar6.py',
-        description='Typical DL lifecycle pipelines.')
-    parser.add_argument('--download', action='store_true', help='Download images and extract to data/raw directory')
-    parser.add_argument('--train', action='store_true', help='Build, train and save model to models/seminar6_model')
-    parser.add_argument('--upload', action='store_true', help='Upload model to S3 storage')
-    args = parser.parse_args()
-    if args.download:
-        download_data()
-    if args.train:
-        train()
-    if args.upload:
-        upload()
+    # download_data()
+    train()
+    upload()
